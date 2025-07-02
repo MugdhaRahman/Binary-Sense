51ab7040ecd524ad342c59da08b6479cfa3405c4
MugdhaRahman <mugdharahman11@gmail.com>
2025-07-02 23:17:05 +0600

    UI

--- app/src/main/java/com/mrapps/binarysense/MainScreen.kt
+++ app/src/main/java/com/mrapps/binarysense/MainScreen.kt
@@ -1,10 +1,16 @@
 package com.mrapps.binarysense
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -12,10 +18,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.mrapps.binarysense.ui.theme.colorPrimaryDark
+import com.mrapps.binarysense.ui.theme.textColor
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -47,12 +58,79 @@ fun MainScreen() {
             }
         }
     ) { innerPadding ->
-        // Content of the main screen goes here
-        Box(modifier = Modifier.padding(innerPadding)) {
-            // Add your content here
+
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(16.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Top
+        ) {
+
+            Card(
+                onClick = { /* handle click */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                elevation = CardDefaults.cardElevation(6.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.background
+                ),
+                border = BorderStroke(1.dp, color = colorPrimaryDark)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        "Metal Detector",
+
+                        color = textColor,
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.bodyLarge,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+
+
+            Card(
+                onClick = { /* handle click */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                elevation = CardDefaults.cardElevation(6.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.background
+                ),
+                border = BorderStroke(1.dp, color = colorPrimaryDark)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        "Barometer",
+
+                        color = textColor,
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.bodyLarge,
+                        fontWeight = FontWeight.Bold
+
+                    )
+                }
+            }
+
         }
     }
+
 }
+
+
 @Preview
 @Composable
 fun PreviewHomeScreen() {
